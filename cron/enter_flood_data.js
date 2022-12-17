62e84e26e4d8e6d35cf2f09ebb7add305571e577
picaso <sjetty786@gmail.com>
2022-12-17 17:13:40 +0530

    Change in package json , cron

--- cron/enter_flood_data.js
+++ cron/enter_flood_data.js
@@ -8,8 +8,9 @@ const options = {
 };
 
 let geocoder = NodeGeocoder(options);
-const path = './flood.xls';
+const path = './tbl_flood_details.xls';
 try {
+	let count = 0;
 	node_xj = require('xls-to-json-lc');
 	node_xj(
 		{
@@ -24,27 +25,27 @@ try {
 			} else {
 				let flood_arr = [];
 				result.map(async (f_data) => {
-					if (f_data.city != '') {
-						let cty_arr = f_data.city.split(',');
-						//console.log("cty length ",cty_arr.length);
-						if (cty_arr.length > 1) {
-							for (i in cty_arr) {
-								await geocoder.geocode(`${cty_arr[i]}`).then((res) => {
-									//console.log('this is resp 1 :', res);
-									let { latitude, longitude } = res;
+					if (f_data.district != '') {
+						let dist_arr = f_data.district.split(',');
+						//console.log("cty length ",dist_arr.length);
+						if (dist_arr.length > 1) {
+								// await geocoder.geocode(`${dist_arr[i]}`).then((res) => {
+								// 	//console.log('this is resp 1 :', res);
+								// 	let { latitude, longitude } = res;
 
 									let floodObj = {
 										date_of_flood: f_data.date_of_flood,
 										state: entities.encode(f_data.state),
-										district: entities.encode(f_data.district),
-										city: entities.encode(cty_arr[i]),
-										latitude: latitude,
-										longitude: longitude,
+										district: entities.encode(dist_arr[0]),
+										city: entities.encode(f_data.city),
+										latitude: f_data.latitude,
+										longitude: f_data.longitud,
 										links: f_data.links,
 									};
 									flood_arr.push(floodObj);
-								});
-							}
+								//});
+							
+							//console.log('flood_arr length : ', flood_arr.length);
 						} else {
 							let floodObj = {
 								date_of_flood: f_data.date_of_flood,
@@ -61,7 +62,7 @@ try {
 						let floodObj = {
 							date_of_flood: f_data.date_of_flood,
 							state: entities.encode(f_data.state),
-							district: entities.encode(f_data.district),
+							district: '',
 							city: entities.encode(f_data.city),
 							latitude: f_data.latitude,
 							longitude: f_data.longitud,
@@ -69,31 +70,32 @@ try {
 						};
 						flood_arr.push(floodObj);
 					}
-					console.log("flood_arr length : ",flood_arr.length );
-					// flood_arr.map((floodObj) => {
-					// 	db.any(
-					// 		`INSERT INTO tbl_flood_details (date_of_flood,state,district,city,latitude,longitude,links) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING id`,
-					// 		[
-					// 			floodObj.date_of_flood,
-					// 			floodObj.state,
-					// 			floodObj.district,
-					// 			floodObj.city,
-					// 			floodObj.latitude,
-					// 			floodObj.longitude,
-					// 			floodObj.links,
-					// 		]
-					// 	)
-					// 		.then((data) => {
-					// 			if (data.length > 0) {
-					// 				count++;
-					// 				console.log(`Total Data Added : ${count}`);
-					// 			}
-					// 		})
-					// 		.catch((err) => {
-					// 			console.log('this is error :', err);
-					// 		});
-					// });
 				});
+				if (flood_arr.length > 0) {
+					flood_arr.map((floodObj) => {
+						db.any(
+							`INSERT INTO tbl_flood_details (date_of_flood,state,district,city,latitude,longitude,links) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING id`,
+							[
+								floodObj.date_of_flood,
+								floodObj.state,
+								floodObj.district,
+								floodObj.city,
+								floodObj.latitude,
+								floodObj.longitude,
+								floodObj.links,
+							]
+						)
+							.then((data) => {
+								if (data.length > 0) {
+									count++;
+									console.log(`Total Data Added : ${count}`);
+								}
+							})
+							.catch((err) => {
+								console.log('this is error :', err);
+							});
+					});
+				}
 			}
 		}
 	);
